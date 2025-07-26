2f47d03b413657d5ba95f7ec5d1d5e802696ad9b
Admin <Admin@TOUHID>
2025-07-26 22:53:17 +0600

    Adding All test method

--- src/test/java/com/project/insurance/service/PolicyServiceTest.java
+++ src/test/java/com/project/insurance/service/PolicyServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -40,24 +41,66 @@ public class PolicyServiceTest {
     }
 
     @Test
-    void testGetAll() {
+    void testGetPolicyAll() {
         List<Policy> policyList = List.of(samplePolicy);
         when(policyRepo.findAll()).thenReturn(policyList);
 
         List<Policy> result = policyService.getPolicyAll();
         assertEquals(1, result.size());
         verify(policyRepo, times(1)).findAll();
+        System.out.println("testGetPolicyAll is complete");
 
     }
 
     @Test
     void testGetPolicyById() {
         when(policyRepo.findById(1L)).thenReturn(Optional.of(samplePolicy));
+
         Policy policy = policyService.getPolicyById(1L);
         assertNotNull(policy);
-        assertEquals("Touhid",policy.getHolderName());
-        verify(policyRepo,times(1)).findById(1L);
+        assertEquals("Touhid", policy.getHolderName());
+        verify(policyRepo, times(1)).findById(1L);
+
+        System.out.println("testGetPolicyById is complete");
     }
 
+    @Test
+    void testGetPolicyByIdNotFound() {
+        when(policyRepo.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> {
+            policyService.getPolicyById(1L);
+        });
+        System.out.println("testGetPolicyByIdNotFound is complete");
+    }
+
+    @Test
+    void testCreatePolicy() {
+        when(policyRepo.save(samplePolicy)).thenReturn(samplePolicy);
+
+        Policy result = policyService.create(samplePolicy);
+        assertEquals("Touhid", result.getHolderName());
+        verify(policyRepo).save(samplePolicy);
+
+        System.out.println("testCreatePolicy is complete");
+    }
+
+    @Test
+    void testUpdatePolicy() {
+        Policy updatePolicy = new Policy();
+        updatePolicy.setHolderName("Ashik");
+        updatePolicy.setType("Health");
+        updatePolicy.setStartDate(LocalDate.now());
+        updatePolicy.setEndDate(LocalDate.now().plusYears(2));
+        updatePolicy.setPremiumAmount(2000.00);
+
+        when(policyRepo.findById(1L)).thenReturn(Optional.of(samplePolicy));
+        when(policyRepo.save(any(Policy.class))).thenReturn(updatePolicy);
+
+        Policy result = policyService.update(1L,updatePolicy);
+        assertEquals("Ashik",result.getHolderName());
+        verify(policyRepo).save(any(Policy.class));
+        System.out.println("testUpdatePolicy is complete");
+    }
 
 }
